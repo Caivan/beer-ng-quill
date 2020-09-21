cbba13b1c53738de45c4813bf33489b38b0f8db4
Caivan <caivan2@gmail.com>
2020-09-20 20:11:19 -0500

    Configure Quill editor and install quill image resize dependence

--- projects/beer-ng-quill/src/lib/beer-ng-quill.module.ts
+++ projects/beer-ng-quill/src/lib/beer-ng-quill.module.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, NgModule, OnInit } from '@angular/core';
+import { AfterViewInit, Input, NgModule, OnInit } from '@angular/core';
 import { BeerNgQuillComponent } from './beer-ng-quill.component';
-
+import Quill from 'quill';
 
 
 @NgModule({
@@ -10,6 +10,13 @@ import { BeerNgQuillComponent } from './beer-ng-quill.component';
   exports: [BeerNgQuillComponent]
 })
 export class BeerNgQuillModule implements OnInit, AfterViewInit { 
+
+  @Input ()
+  options: any;
+
+  @Input ()
+  idEditor: string;
+  
   constructor() { 
     
   }
@@ -19,6 +26,31 @@ export class BeerNgQuillModule implements OnInit, AfterViewInit {
    
   }
   ngAfterViewInit(): void {
+    
+  }
+
+  configQuill () {
+
+    let opts = {
+      debug: 'info',
+      modules: {
+        toolbar:  [
+          [{ header: [1, 2, false] }],
+          ['bold', 'italic', 'underline'],
+          ['image', 'code-block']
+        ],
+        imageResize: {}
+      },
+      placeholder: 'Compose an epic...',
+      readOnly: false,
+      theme: 'snow'
+    };
+
+    if (this.options) {
+      opts = this.options;
+    }
+        
+    const editor = new Quill(('#'+this.idEditor), opts);
     
   }
 }
